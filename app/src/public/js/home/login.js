2cc4b9d91aa08ca67c45aacd843e22fda2cf130b
elee1101 <123059075+elee1101@users.noreply.github.com>
2024-10-29 14:05:24 -0700

    create fecth

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,7 +11,14 @@ const id = document.querySelector("#id"),
   function login() {
     const req = {
       id: id.value,
-      psword: psword.value
+      psword: psword.value,
     };
-    console.log(req)
+
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    });
   }

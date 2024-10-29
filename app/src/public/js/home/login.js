348e1e59271e1e02c10b50ceae4685e513a44193
elee1101 <123059075+elee1101@users.noreply.github.com>
2024-10-29 16:26:00 -0700

    login function frontend

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,5 +23,14 @@ const id = document.querySelector("#id"),
       },
       body: JSON.stringify(req),
     }).then((res) => res.json())
-      .then(console.log);
+      .then((res) => {
+        if (res.success) {
+          location.href = "/";
+        } else {
+          alert(res.msg);
+        }
+      })
+      .catch((err) => {
+        console.error(new Error("Error"));
+      })
   }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,8 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+
+//login 정보 전달되는 경로?
 router.post("/login", ctrl.process.login);
 
 //router를 사용할 수 있도록 외부로 export

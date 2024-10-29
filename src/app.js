a0caa2721a2dd68cd848b1cb4eba2d1b147e469e
elee1101 <123059075+elee1101@users.noreply.github.com>
2024-10-29 02:26:10 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+"use strict";
+//will use ECMAScript
+
+// express 사용하기!
+// modle
+const express = require("express");
+const app = express();
+
+//routing
+const home = require("./routes/home")
+
+//app setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use -> 미들 웨어를 등록해주는 method
+
+module.exports = app;
+
+
+//npm init -v package.json (default) 만들기
+
+
+
+// express 사용 안 할시 코드 복잡
+
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.end("This is the route");
+//   } else if (req.url === '/login') {
+//     res.end("login page");
+//   }
+// });
+
+// app.listen(3001, () => {
+//   console.log("http server")
+// })
+
+
+
+
+

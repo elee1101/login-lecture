7bac1c91a84f0c0b616b23134e2147fa7818cf8f
elee1101 <123059075+elee1101@users.noreply.github.com>
2024-10-29 19:49:07 -0700

    sign up page

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 
+router.get("/register", ctrl.output.register);
 //login 정보 전달되는 경로?
 router.post("/login", ctrl.process.login);
 
